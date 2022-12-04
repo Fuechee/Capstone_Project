497f06b57714ac73419689810cd3b0f239de0873
Fuechee <f9vang@gmail.com>
2022-12-04 13:16:00 -0600

    Fixed database issues

--- backend/customers/serializers.py
+++ backend/customers/serializers.py
@@ -4,5 +4,5 @@ from .models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'first_name','last_name','email','address','city','zip_code','state','mobile_number','product_id']
+        fields = ['id', 'first_name','last_name','email','address','city','zip_code','state','mobile_number','product']
         depth = 1
