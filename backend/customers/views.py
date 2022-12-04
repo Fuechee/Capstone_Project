20eaa6f71c74453d34a31262817c91255b015cf2
Fuechee <f9vang@gmail.com>
2022-12-04 11:33:48 -0600

    Created api for registered customers

--- backend/customers/views.py
+++ backend/customers/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CustomerSerializer
 from .models import Customer
 
 # Create your views here.
-@api_view(['GET','POST'])
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def customers_list(request):
-    if request.method == 'GET':
-        query_set = Customer.objects.all()
-        serializer = CustomerSerializer(query_set, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
+    customers = Customer.objects.all()
+    serializer = CustomerSerializer(customers, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def registered_customers(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
         serializer = CustomerSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_vaild():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        customers = Customer.objects.filter(user_id=request.user.id)
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
