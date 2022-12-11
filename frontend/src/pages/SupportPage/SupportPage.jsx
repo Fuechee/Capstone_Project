2f866cdedd70064e161a4c1e7d35353b7725cc1c
Fuechee <f9vang@gmail.com>
2022-12-11 14:08:00 -0600

    Finished creating the main support page

--- frontend/src/pages/SupportPage/SupportPage.jsx
+++ frontend/src/pages/SupportPage/SupportPage.jsx
@@ -7,8 +7,11 @@ const formReducer = (state, event) => {
     }
 }
 const SupportPage = () => {
-    const [submitting, setSubmitting] = useState([false])
-    const [formData, setFormData] = useReducer(formReducer, {})
+    const [submitting, setSubmitting] = useState(false);
+    const [formData, setFormData] = useReducer(formReducer, {
+        count: 100,
+    });
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -16,6 +19,7 @@ const SupportPage = () => {
         setTimeout(() => {
             setSubmitting(false);
         }, 3000)
+        alert("Thank you for submitting a form, we'll get back to you as soon as possible")
     }
 
     const handleChange = event => {
@@ -27,98 +31,72 @@ const SupportPage = () => {
     
     return ( 
         <div className="background">
-            <div className="issues">
-                <h1>What's The Problem?</h1>
+            <div className="contact">
+                <h1>Contact Us</h1>
                 <form onSubmit={handleSubmit}>
-                {submitting &&
+                {submitting && 
                     <div>Submtting Form...</div>
                 }
-                    <fieldset>
+                    <fieldset disabled={submitting}>
                         <label>
-                            <h3>Name</h3>
-                            <input name="name" onChange={handleChange}/>
+                            <h3>What's the Problem?</h3>
+                            <select name="option" onChange={handleChange} value={formData.option || ''}>
+                                <option value="">--Please choose an option--</option>
+                                <option value="online">Online Order</option>
+                                <option value="product">Product Problems</option>
+                                <option value="profile">Profile Problems</option>
+                            </select>
+                        </label>
+                        <label>
+                            <select name="product" disabled={formData.question !== 'Product Problems'} onChange={handleChange} value={formData.product || ''}>
+                                <option value="">--What's the product?--</option>
+                                <option value="product">Momentum 4 Wireless</option>
+                                <option value="profile">HD 540BT</option>
+                                <option value="profile">HD 450BT</option>
+                                <option value="profile">HD 350BT</option>
+                                <option value="profile">HD 250BT</option>
+                                <option value="profile">HD 800 S</option>
+                                <option value="profile">HD 650</option>
+                                <option value="profile">HD 600</option>
+                                <option value="profile">HD 560S</option>
+                                <option value="profile">CX Plus SE True Wireless</option>
+                                <option value="profile">Momentum True Wireless 3</option>
+                                <option value="profile">CX True Wireless</option>
+                                <option value="profile">Sport True Wireless</option>
+                            </select>
+                        </label>
+                        <label>
+                            <input name="order" disabled={formData.question !== 'Online Order'} placeholder="Do You Have An Order Number?" onChange={handleChange} value={formData.order || ''}/>
                         </label>
                     </fieldset>
-                    <button type="submit">Submit</button>
+                    <fieldset disabled={submitting}>
+                        <label>
+                            <h3>Your Personal Information</h3>
+                            <input name="first_name" placeholder="First Name" onChange={handleChange} value={formData.first_name || ''}/>
+                        </label>
+                        <label>
+                            <input name="last_name" placeholder="First Name" onChange={handleChange} value={formData.last_name || ''}/>
+                        </label>
+                        <label>
+                            <input name="email" placeholder="Email" onChange={handleChange} value={formData.email || ''}/>
+                        </label>
+                        <label>
+                            <input name="address" placeholder="Address" onChange={handleChange} value={formData.address || ''}/>
+                        </label>
+                        <label>
+                            <input name="city" placeholder="City" onChange={handleChange} value={formData.city || ''}/>
+                        </label>
+                        <label>
+                            <input name="state" placeholder="State" onChange={handleChange} value={formData.state || ''}/>
+                        </label>
+                        <label>
+                            <input name="zip_code" placeholder="Zip Code" onChange={handleChange} value={formData.zip_code || ''}/>
+                        </label>
+                        
+                    </fieldset>
+                    <button type="submit" disabled={submitting}>Submit</button>
                 </form>
-        </div>
-                {/* <form className="problem">
-                    <label>
-                        <select >
-                            <option value="question">What's your question?</option>
-                            <option value="online">Online Order</option>
-                            <option value="product">Product Problems</option>
-                            <option value="profile">Profile Problems</option>
-                        </select>
-                    </label>
-                    <label>
-                        <select >
-                            <option value="product-type">What's the product type?</option>
-                            <option value="headphones">Headphones</option>
-                            <option value="earphones">Earphones</option>
-                        </select>
-                    </label>
-                    <label>
-                        <select >
-                            <option value="product">What's the product?</option>
-                            <option value="product">Momentum 4 Wireless</option>
-                            <option value="profile">HD 540BT</option>
-                            <option value="profile">HD 450BT</option>
-                            <option value="profile">HD 350BT</option>
-                            <option value="profile">HD 250BT</option>
-                            <option value="profile">HD 800 S</option>
-                            <option value="profile">HD 650</option>
-                            <option value="profile">HD 600</option>
-                            <option value="profile">HD 560S</option>
-                            <option value="profile">CX Plus SE True Wireless</option>
-                            <option value="profile">Momentum True Wireless 3</option>
-                            <option value="profile">CX True Wireless</option>
-                            <option value="profile">Sport True Wireless</option>
-                        </select>
-                    </label>
-                    <label>
-                        <input 
-                        type="text" 
-                        name="name"
-                        placeholder="Do You Have An Order Number?"
-                        onChange={handleChange}
-                        />
-                    </label>
-                </form> */}
-            
-            {/* <div className="details">
-                <h1>Your Personal Details</h1>
-                <fieldset className="contact">
-                    <label>First Name:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>Last Name:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>Email:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>Address:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>Zip Code:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>City:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>State:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>Phone Number:  
-                        <input type="text" name="name" />
-                    </label>
-                    <label>Your Message:
-                    <textarea value={textarea} onChange={handleChange} />
-                    </label>
-                </fieldset>
-                
-            </div> */}
+            </div>
         </div>
      );
 }
